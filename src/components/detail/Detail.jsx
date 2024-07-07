a4def7d4e972604107d29d2832cfeb1d484969a0
kelum <ugdn.kelumpriya@gmail.com>
2024-07-08 05:04:37 +0530

    user details structure - front end

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,69 @@
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Dileepa Kelumpriya</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Privacy and help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItems">
+              <div className=" photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>my background</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItems">
+              <div className=" photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>my background</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItems">
+              <div className=" photoDetail">
+                <img src="./bg.jpg" alt="" />
+                <span>my background</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Share Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block The User</button>
+        <button className="logOutBtn">Log Out</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
